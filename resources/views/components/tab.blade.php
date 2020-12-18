51cda106837df802ad201bfa2429d38bb30fe76a
Ian Travers <mr.itpe@gmail.com>
2020-12-18 15:02:39 +0300

    Encapsulate the showIfActive method into tab component

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -1,8 +1,15 @@
 @props(['name'])
 
 <div
-    x-data="{ 'name': '{{ $name }}', 'show': false }"
+    x-data="{
+        name: '{{ $name }}',
+        show: false,
+        showIfActive(active) {
+            this.show = (this.name === active)
+        }
+    }"
     x-show="show"
+
 >
     {{ $slot }}
 </div>
--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -7,7 +7,7 @@
         tabHeadings: [],
         toggleTabs() {
             this.tabs.forEach(
-                tab => tab.__x.$data.show = tab.__x.$data.name === this.activeTab
+                tab => tab.__x.$data.showIfActive(this.activeTab)
             );
         },
     }"
